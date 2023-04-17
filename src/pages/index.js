e48b97c40ffbf798a8f39b42ddefdf6f7f91fbcb
Klaudiusz Skowroński <klaudsys@gmail.com>
2023-04-17 20:31:11 +0200

    added axio api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
+import axios from '@/helpers/axios'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({heroes, isRequestFaild}) {
+  
+  if (isRequestFaild) {
+    return <p>Ooops something went wrong ....</p>
+  }
+
+  const herosElements = heroes.map( hero => <HeroElement key={hero.id} {...hero} />)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -12,10 +20,16 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/pages/index.js</code>
         </p>
+        <div>
+          <h1>HERO ENCYCLOPEDIA</h1>
+          <ul>
+            {herosElements}
+          </ul>
+        </div>
         <Link href="/memes">
           <a>Memey --- </a>
         </Link>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        {/* <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
@@ -32,10 +46,10 @@ export default function Home() {
               priority
             />
           </a>
-        </div>
+        </div> */}
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+      {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
@@ -44,7 +58,7 @@ export default function Home() {
           height={37}
           priority
         />
-      </div>
+      </div> */}
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
@@ -125,4 +139,41 @@ export default function Home() {
       </div>
     </main>
   )
+}
+
+function HeroElement({id, name, url}) {
+  return (
+    <li>
+      <Link href={`/hero/${id}`}>
+      <a>
+        <img alt={`Photo of ${name}`} src={url}/>
+        <p>{name}</p>
+      </a>
+      </Link>
+    </li>
+  )
+}
+
+
+export async function getStaticProps() {
+
+  const { data, status } = await axios.get('/search/a')
+
+  if (status !== 200) {
+    return {
+      props: {
+        isRequestFaild: true,
+      }
+    }
+  }
+
+  const { results } = data
+  const heroes = results.map(({id, name, image: { url }}) => ({id, name, url}))
+
+  return {
+    props: {
+      heroes,
+      isRequestFaild: false,
+    }
+  }
 }
